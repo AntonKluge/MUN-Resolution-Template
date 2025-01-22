f9c2231744df837c3fd42092c5cdd8d4509650fe
AntonKluge <anton.kluge@mailbox.org>
2025-01-22 11:47:40 +0100

    robus load script

--- images/load_un_logo.sh
+++ images/load_un_logo.sh
@@ -1,1 +1,12 @@
-curl -o un_logo.png https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Logo_of_the_United_Nations_%28B%26W%29.svg/902px-Logo_of_the_United_Nations_%28B%26W%29.svg.png
+#!/bin/bash
+
+UN_LOGO_URL="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Logo_of_the_United_Nations_%28B%26W%29.svg/902px-Logo_of_the_United_Nations_%28B%26W%29.svg.png"
+
+# Check if we're in the images directory or root directory
+if [ "${PWD##*/}" = "images" ]; then
+    OUTPUT_PATH="un_logo.png"
+else
+    OUTPUT_PATH="./images/un_logo.png"
+fi
+
+curl -o "$OUTPUT_PATH" "$UN_LOGO_URL"
